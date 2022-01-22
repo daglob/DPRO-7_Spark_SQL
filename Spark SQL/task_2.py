ef63eae4183f5500f335ba026d1489ab96cfb075
daglob <daglob@amil.ru>
2022-01-22 16:30:55 +0300

    fix

--- Spark SQL/task_2.py
+++ Spark SQL/task_2.py
@@ -8,29 +8,31 @@ from pyspark.sql import functions as F
 
 spark = pySparkSql.SparkSession.builder.appName('PysparkSQL-HW_Task-1').master('local').getOrCreate()
 data = spark.read.csv('/opt/bitnami/spark/volume/Spark SQL/owid-covid-data.csv', header=True, inferSchema=True)
-window = Window().partitionBy('location')
-data.withColumn('max_new_cases', F.max('new_cases').over(window)) \
-    .select(F.to_date(F.col('date')).alias('date'),
-            'location',
-            'new_cases') \
+filtered_data = data.select(F.to_date(F.col('date')).alias('date'),
+                            'location',
+                            'new_cases') \
     .where(F.col('date').between('2021-03-24', '2021-03-31')) \
-    .where(F.col('location') != 'World') \
+    .where(F.col('location') != 'World')
+
+window = Window().partitionBy('location')
+filtered_data.withColumn('max_new_cases', F.max('new_cases').over(window)) \
     .where(F.col('max_new_cases') == F.col('new_cases')) \
     .orderBy(F.col('max_new_cases').desc()) \
+    .drop('new_cases') \
     .show(10)
 
-# +----------+--------------------+---------+
-# |      date|            location|new_cases|
-# +----------+--------------------+---------+
-# |2021-03-25|              Brazil| 100158.0|
-# |2021-03-26|             Hungary|  11265.0|
-# |2021-03-30|              Serbia|   9983.0|
-# |2021-03-30|              Greece|   4322.0|
-# |2021-03-29|              Kosovo|   4257.0|
-# |2021-03-29|Bosnia and Herzeg...|   3755.0|
-# |2021-03-24|             Moldova|   2273.0|
-# |2021-03-26|               Kenya|   2008.0|
-# |2021-03-31|     North Macedonia|   1511.0|
-# |2021-03-25|       Cote d'Ivoire|    767.0|
-# +----------+--------------------+---------+
+# +----------+--------------+-------------+
+# |      date|      location|max_new_cases|
+# +----------+--------------+-------------+
+# |2021-03-24|        Europe|     256192.0|
+# |2021-03-31|European Union|     216452.0|
+# |2021-03-31|          Asia|     183350.0|
+# |2021-03-25| South America|     148476.0|
+# |2021-03-24| North America|     100760.0|
+# |2021-03-25|        Brazil|     100158.0|
+# |2021-03-24| United States|      86960.0|
+# |2021-03-31|         India|      72330.0|
+# |2021-03-24|        France|      65392.0|
+# |2021-03-31|        Turkey|      39302.0|
+# +----------+--------------+-------------+
 # only showing top 10 rows
